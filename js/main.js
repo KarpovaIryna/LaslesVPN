cc1105bf834c962d994a74e8b729da266e6cb749
Karpova <karpovai@outlook.com>
2020-12-06 20:07:14 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-;(function() {
-	"use strict";
+"use strict";
+$(".hamburger").click(function (e) {
+	e.preventDefault();
+	$(".ba-nav").toggleClass('is-active');
+	$(this).toggleClass('is-active');
+	$(".ba-header").toggleClass('is-active');
+});
+$('ul li a').click(function () {
+	if ($('.ba-nav').hasClass('is-active')) {
+		$('.ba-nav').removeClass('is-active');
+		$('.hamburger').removeClass('is-active');
+		$('.ba-header').removeClass('is-active');
+	}
+});
  
-})();
 
